44f3f5d7f936ca8aae2f71f903e726e06ecf284f
Marc Cordova <mcordova47@gmail.com>
2022-05-07 23:56:39 -0400

    Add types

--- src/Main.purs
+++ src/Main.purs
@@ -20,14 +20,20 @@ main = HA.runHalogenAff do
 
 data Action = Increment | Decrement
 
+type State = Int
+
 component :: ∀ query input output m. Component query input output m
 component =
   H.mkComponent
-    { initialState: const 0
+    { initialState
     , render
     , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
     }
   where
+    initialState :: input -> State
+    initialState _ = 0
+
+    render :: State -> H.ComponentHTML Action () m
     render state =
       HH.div_
         [ HH.button [ HE.onClick \_ -> Decrement ] [ HH.text "-" ]
@@ -35,6 +41,7 @@ component =
         , HH.button [ HE.onClick \_ -> Increment ] [ HH.text "+" ]
         ]
 
+    handleAction :: Action -> H.HalogenM State Action () output m Unit
     handleAction = case _ of
       Increment -> H.modify_ (_ + 1)
       Decrement -> H.modify_ (_ - 1)
